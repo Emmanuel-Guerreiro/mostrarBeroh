5fa6180b1c1302fbf40d89266a78dfeda1c395c1
Emmanuel Guerreiro <emmanuelguerreiro98@gmail.com>
2020-12-29 13:09:35 -0300

    arreglar orientacion

--- src/componentes/tarjeta.js
+++ src/componentes/tarjeta.js
@@ -6,27 +6,12 @@ import BotonBorrar from './botonBorrar';
 
 const Tarjeta = ({tour, listaTour, setListaTour, setReloadButton}) => {
     return(
-<<<<<<< HEAD
-        <div className="card" style={{width: "35rem"}}>
-            <img className="card-img-top" src="tour.image" alt="tour.id"/>
-            <div className="card-body">
-                <table>
-                    <tr>
-                        <th><h5 className="card-title justify-content">{tour.name}</h5></th>
-                        <th><span className="ml-5 pl-5">{tour.price}</span></th>
-                    </tr>
-                </table>
-                <p className="card-text text-justify m-4">
-                    {tour.info}
-                </p>
-=======
         <div>
             <img src={tour.image} alt={tour.id}/>
             <div>
                 <h4>{tour.name}</h4>
                 <span>${tour.price}</span>
                 <p>{tour.info}</p>
->>>>>>> 561f8aba81681131192aff07d9360122442c8a51
             </div> 
             <BotonBorrar listaTour={listaTour} setListaTour={setListaTour}
              tour={tour} setReloadButton={setReloadButton}/>
